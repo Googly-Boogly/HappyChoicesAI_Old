746f7aea6e5013a01729da10d20367483b5d3bd0
Googly-Boogly <mainalextaylor@gmail.com>
2024-04-18 00:38:38 -0500

    REAL INIT

--- src/ai_backend/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import consumers
-
-websocket_urlpatterns = [
-    path('ws/audio/tts', consumers.AudioConsumer.as_asgi()),
-    path('ws/audio/stt', consumers.SpeechToTextConsumer.as_asgi()),
-]
